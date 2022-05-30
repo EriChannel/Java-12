f105b94c63384b235f1b2de91df7f638c2e6d7ca
Lập trình cùng Eri <erichannel96@gmail.com>
2022-05-30 20:07:58 +0700

    buổi 1

---
+++ Lesson_01/first_app/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public int age;
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int point;
+    }
+}
---
+++ Lesson_01/hello_world/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào");
+        System.out.println("Tôi tên là Ngọc");
+        System.out.println(20+20);
+        System.out.println('a');
+
+        int age; //Khai báo biến age
+        age = 26;
+
+        long salary = 15000000L;
+
+        System.out.println("Lương của tôi là: " + salary);
+
+        float height = 1.5f;
+        double weight = 40;
+
+        double bmi = weight/(height*height);
+        System.out.println("Chỉ số bmi của tôi: " + bmi);
+
+        char ch = '6';
+        System.out.println(ch);
+
+        boolean flag = 5 < 6;
+        System.out.println(flag);
+    }
+}
